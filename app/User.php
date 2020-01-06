f48983db36bc986d59dfedfb93f6f23f5d69b23d
christanplaza <christan.shane.plaza@sun-asterisk.com>
2020-01-06 14:46:31 +0800

    Slack Integration

---
+++ app/User.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App;
+
+use App\Notifications\NewUserRegistered;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        self::created(function($model) {
+            $model->notify(new NewUserRegistered());
+        });
+    }
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function routeNotificationFor($driver)
+    {
+        return config('app.slack_webhook');
+    }
+}
